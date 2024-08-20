7f4ef0d1a6f8971b22d1c927960b538ff59e82e8
Rajan Kumar <rajan.kumar@cyncly.com>
2024-08-20 15:41:48 +0530

    3D modeling

---
+++ cube.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <fstream>
+
+int main() {
+    // Open a file in write mode
+    std::ofstream stlFile("cube.stl");
+
+    // Check if the file is opened successfully
+    if (!stlFile) {
+        std::cerr << "Unable to open file";
+        return 1; // Return an error code
+    }
+
+    // Write the STL content
+    stlFile << "solid cube\n";
+    
+    stlFile << "  facet normal 0 0 0\n"
+            << "    outer loop\n"
+            << "      vertex 0 0 0\n"
+            << "      vertex 1 0 0\n"
+            << "      vertex 1 1 0\n"
+            << "    endloop\n"
+            << "  endfacet\n";
+
+    stlFile << "  facet normal 0 0 0\n"
+            << "    outer loop\n"
+            << "      vertex 0 0 0\n"
+            << "      vertex 1 1 0\n"
+            << "      vertex 0 1 0\n"
+            << "    endloop\n"
+            << "  endfacet\n";
+
+    stlFile << "  facet normal 0 0 0\n"
+            << "    outer loop\n"
+            << "      vertex 0 0 0\n"
+            << "      vertex 0 1 0\n"
+            << "      vertex 0 1 1\n"
+            << "    endloop\n"
+            << "  endfacet\n";
+
+    stlFile << "  facet normal 0 0 0\n"
+            << "    outer loop\n"
+            << "      vertex 0 0 0\n"
+            << "      vertex 0 1 1\n"
+            << "      vertex 0 0 1\n"
+            << "    endloop\n"
+            << "  endfacet\n";
+
+    stlFile << "  facet normal 0 0 0\n"
+            << "    outer loop\n"
+            << "      vertex 0 0 0\n"
+            << "      vertex 0 0 1\n"
+            << "      vertex 1 0 1\n"
+            << "    endloop\n"
+            << "  endfacet\n";
+
+    stlFile << "  facet normal 0 0 0\n"
+            << "    outer loop\n"
+            << "      vertex 0 0 0\n"
+            << "      vertex 1 0 1\n"
+            << "      vertex 1 0 0\n"
+            << "    endloop\n"
+            << "  endfacet\n";
+
+    stlFile << "  facet normal 0 0 0\n"
+            << "    outer loop\n"
+            << "      vertex 1 1 1\n"
+            << "      vertex 0 1 1\n"
+            << "      vertex 0 0 1\n"
+            << "    endloop\n"
+            << "  endfacet\n";
+
+    stlFile << "  facet normal 0 0 0\n"
+            << "    outer loop\n"
+            << "      vertex 1 1 1\n"
+            << "      vertex 0 0 1\n"
+            << "      vertex 1 0 1\n"
+            << "    endloop\n"
+            << "  endfacet\n";
+
+    stlFile << "  facet normal 0 0 0\n"
+            << "    outer loop\n"
+            << "      vertex 1 1 1\n"
+            << "      vertex 1 0 0\n"
+            << "      vertex 1 0 1\n"
+            << "    endloop\n"
+            << "  endfacet\n";
+
+    stlFile << "  facet normal 0 0 0\n"
+            << "    outer loop\n"
+            << "      vertex 1 1 1\n"
+            << "      vertex 1 1 0\n"
+            << "      vertex 1 0 0\n"
+            << "    endloop\n"
+            << "  endfacet\n";
+
+    stlFile << "  facet normal 0 0 0\n"
+            << "    outer loop\n"
+            << "      vertex 1 1 1\n"
+            << "      vertex 0 1 0\n"
+            << "      vertex 1 1 0\n"
+            << "    endloop\n"
+            << "  endfacet\n";
+
+    stlFile << "  facet normal 0 0 0\n"
+            << "    outer loop\n"
+            << "      vertex 1 1 1\n"
+            << "      vertex 0 1 1\n"
+            << "      vertex 0 1 0\n"
+            << "    endloop\n"
+            << "  endfacet\n";
+
+    stlFile << "endsolid\n";
+
+    // Close the file
+    stlFile.close();
+
+    std::cout << "cube.stl file has been generated." << std::endl;
+
+    return 0;
+}
